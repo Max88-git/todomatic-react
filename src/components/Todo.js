ccdd45ce105bc54a908b9fbf309fa9c0a7be2cac
Max Lockwood <mlockwood@hotmail.co.uk>
2021-09-24 19:20:00 +0100

    More robust focus management

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
 
   const editFieldRef = useRef(null);
   const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -88,7 +98,7 @@ export default function Todo(props) {
     } else {
       editButtonRef.current.focus();
     }
-  }, [isEditing]);
+  }, [wasEditing, isEditing]);
 
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
